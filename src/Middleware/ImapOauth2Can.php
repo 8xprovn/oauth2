1ed47057f161bda7f91800ea1a16c698723ea648
Nam tran <provn8x@gmail.com>
2021-08-04 11:35:13 +0700

    remove authorise

--- src/Middleware/ImapOauth2Can.php
+++ src/Middleware/ImapOauth2Can.php
@@ -19,33 +19,6 @@ class ImapOauth2Can extends ImapOauth2Authenticated
      */
     public function handle($request, Closure $next, ...$guards)
     {
-        $allowed_permissions = ImapOauth2Web::getPermissionUser();
-        if (!$allowed_permissions) {
-            abort(403);
-        }
-        $is_superadmin = (!empty($allowed_permissions['role']) && $allowed_permissions['role'] == 'superadmin') ? true : false;
-        $current_nameas = \Request::route()->getName(); //router name
-        \Gate::before(function () use ($is_superadmin) {
-            //return true;
-            if($is_superadmin){
-                return $is_superadmin;
-            }
-        });
-        if(!empty($allowed_permissions)){
-            \Gate::define('home', function ($user) {
-                return true;
-            });
-            foreach($allowed_permissions as $allowed_permission) {
-                \Gate::define($allowed_permission, function ($user) {
-                    return true;
-                });
-            }
-            
-        }
-        if(\Gate::allows($current_nameas)){
-            return $next($request);
-        }
-
-        abort(403);
+        
     }
 }
